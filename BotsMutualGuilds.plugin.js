b3809c5d7d8a5a8971f5554955a9c829b1ea9027
Eduardo Londero <eduardolonderovieira@gmail.com>
2017-12-22 14:01:40 -0200

    Updated to 1.0.3, fixing tabs

--- BotsMutualGuilds.plugin.js
+++ BotsMutualGuilds.plugin.js
@@ -16,7 +16,7 @@ class BotsMutualGuilds {
   }
 
   getVersion() {
-    return '1.0.2';
+    return '1.0.3';
   }
 
   getAuthor() {
@@ -94,11 +94,8 @@ class BotsMutualGuilds {
         return;
 
     let oldGuilds;
-
-    $('.inner-1_1f7b')
-      .find('.tab-bar-item')
-      .eq(2)
-      .remove();
+    
+    $('div[class*="topSection"').children('div').first().append(`<div class="tabBarContainer-3RVZc-"><div class="tab-bar tabBar-2z50ux TOP"><div class="tab-bar-item tabBarItem-1Lg555 selected">${this.locale.infos}</div><div class="tab-bar-item tabBarItem-1Lg555">${this.locale.guild}</div></div></div>`);
 
     const tabs = $('.inner-1_1f7b')
       .find('.tab-bar-item');
@@ -129,9 +126,9 @@ class BotsMutualGuilds {
           .sort((a, b) => a.name.toLowerCase() > b.name.toLowerCase() ? 1 : -1)
           .forEach((guild) => {
             guilds.find('.scroller-fzNley').append(
-              $(`<div class="listRow-1wEi-U weightMedium-13x9Y8">
+              $(`<div class="listRow-1wEi-U flex-3B1Tl4 weightMedium-13x9Y8">
                   ${this.getIconTemplate(guild)}
-                  <div class="guild-inner">
+                  <div class="listRowContent-3pOfag">
                     <div class="listName-1Xr1Jk size16-3IvaX_ height16-1qXrGy">${guild.name}</div>
                     <div class="guildNick-1oZKE3 weightNormal-3gw0Lm">${this.getUser(guild, user).nick || ''}</div>
                   </div>
@@ -148,5 +145,111 @@ class BotsMutualGuilds {
 
         guilds.append(oldGuilds);
       });
+  }
+  
+  get locale() {
+    switch (document.documentElement.getAttribute('lang').split('-')[0]) {
+      case 'en': // English
+      case 'da': // Dansk
+        return {
+          'infos': 'User Info',
+          'guild': 'Mutual Servers',
+        };
+      case 'hr': // Croatian
+        return {
+          'infos': 'Informacije o korisniku',
+          'guild': 'Zajednički serveri',
+        };
+      case 'de': // Deutsch
+        return {
+          'infos': 'Benutzerinformationen',
+          'guild': 'Gemeinsame Server',
+        };
+      case 'es': // Español
+        return {
+          'infos': 'Información de usuario', 
+          'guild': 'Servidores en común',
+        };
+      case 'fr': // Français
+        return {
+          'infos': 'Infos utilisateur', 
+          'guild': 'Serveurs en commun',
+        };
+      case 'it': // Italiano
+        return {
+          'infos': 'Dati personali', 
+          'guild': 'Server in comune',
+        };
+      case 'nl': // Nederlands
+        return {
+          'infos': 'Gebruikersinformatie', 
+          'guild': 'Gemeenschappelijke servers',
+        };
+      case 'no': // Norsk
+        return {
+          'infos': 'User Info', 
+          'guild': 'Felles servere',
+        };
+      case 'pl': // Polski
+        return {
+          'infos': 'Informacje', 
+          'guild': 'Wspólne serwery',
+        };
+      case 'pt': // Português do Brasil
+        return {
+          'infos': 'Informações do usuário', 
+          'guild': 'Servidores em comum',
+        };
+      case 'fi': // Suomi
+        return {
+          'infos': 'Käyttäjätiedot', 
+          'guild': 'Yhteiset palvelimet',
+        };
+      case 'sv': // Svenska
+        return {
+          'infos': 'Användarinfo', 
+          'guild': 'Gemensamma servrar',
+        };
+      case 'tr': // Türkçe
+        return {
+          'infos': 'Kullanıcı Bilgisi', 
+          'guild': 'Ortak Sunucular',
+        };
+      case 'cs': // Čeština
+        return {
+          'infos': 'Údaje uživatele', 
+          'guild': 'Společné servery',
+        };
+      case 'bg': // български
+        return {
+          'infos': 'User Info', 
+          'guild': 'Общи сървъри',
+        };
+      case 'ru': // Русский
+        return {
+          'infos': 'Профиль', 
+          'guild': 'Общие сервера',
+        };
+      case 'uk': // Український
+        return {
+          'infos': 'User Info', 
+          'guild': 'Спільні сервери',
+        };
+      case 'ja': // 日本語
+        return {
+          'infos': 'ユーザー情報', 
+          'guild': '共通のサーバー',
+        };
+      case 'zh': // 繁體中文
+        return {
+          'infos': '使用者資訊', 
+          'guild': '共同的伺服器',
+        };
+      case 'ko': // 한국어 
+        return {
+          'infos': '사용자 정보', 
+          'guild': '같이 있는 서버',
+        };
+    }
   }
 }
